a3e728608b3e5cf3699fbdde29c8cdbb8a1ac07e
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2023-02-07 15:53:06 +0100

    [egalisation] Améliore le layout

--- egalisation.m
+++ egalisation.m
@@ -53,28 +53,42 @@ function NGegalisation = egalisation (I)
     % ---------
     figure;
 
-    subplot(3, 2, 1); %sélectionne le premier cadran de la fenêtre
+    subplot(4, 2, 1); %sélectionne le premier cadran de la fenêtre
     imshow(I);
     %title(strcat(['min = ', num2str(amin), ' max = ', num2str(amax)])); % a la fin du subplot
 
-    subplot(3, 2, 2); %sélectionne le deuxieme cadran de la fenêtre
+    subplot(4, 2, 2); %sélectionne le deuxieme cadran de la fenêtre
     imshow(Iegalisation)
     %title(strcat(['min = ', num2str(min(min(Ietirement))), ' max = ', num2str(max(max(Ietirement)))])); % a la fin du subplot
 
-    subplot(3, 2, 3); %sélectionne le troisème cadran de la fenêtre
+    subplot(4, 2, 3); %sélectionne le troisème cadran de la fenêtre
     imhist(I);
+    axis([-inf +inf -inf +inf]); %spécifie l'axis par défaut, sinon ça utilise l'axis du LUT
+    title("Histogramme image base (axes complets)");
 
-    subplot(3, 2, 4); %sélectionne le quatrième cadran de la fenêtre
+    subplot(4, 2, 4); %sélectionne le quatrième cadran de la fenêtre
     imhist(Iegalisation);
+    axis([-inf +inf -inf +inf]); %spécifie l'axis par défaut, sinon ça utilise l'axis du LUT
+    title("Histogramme image égalisée (axes complets)");
 
-    subplot(3, 2, 5); %sélectionne le cinquième cadran de la fenêtre
+    subplot(4, 2, 5); %sélectionne le cinquième cadran de la fenêtre
+    imhist(I);
+    title("Histogramme image base (axes tronqués)");
+
+    subplot(4, 2, 6); %sélectionne le sixième cadran de la fenêtre
+    imhist(Iegalisation);
+    title("Histogramme image égalisée (axes tronqués)");
+
+    subplot(4, 2, 7); %sélectionne le cinquième cadran de la fenêtre
     plot(LUTng);
     axis([1 256 0 255]);
     xlabel('NG entrée');
     ylabel('NG sortie');
+    title("LUT");
 
-    subplot(3, 2, 6); %sélectionne le sixième cadran de la fenêtre
+    subplot(4, 2, 8); %sélectionne le sixième cadran de la fenêtre
     plot(HCNcumule);
+    title("HCN de l'image égalisée");
     axis([1 256 0 1]);
 
 end
