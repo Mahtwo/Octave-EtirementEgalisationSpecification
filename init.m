cbaa72b7411b07c002c91c42c3ef40a20f0cb032
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2023-02-07 22:18:40 +0100

    [init] si pas de paramètre retourne imread lena_fc.png et outils.png

--- init.m
+++ init.m
@@ -1,13 +1,18 @@
-function im = init(filename);
+function [im1, im2] = init(filename);
     pkg load image
     % si pas de fichier specifié, prends image_test_bruit.tif
     if not(exist('filename')) || isempty(filename)
-        im = imread('image_test_bruit.tif');
+        im1 = imread('lena_fc.png');
+        im2 = imread('outils.png');
     else
-        im = imread(filename);
+        im1 = imread(filename);
     end
-    [nbLignes, nbColonnes, can] = size(im); % m=nb lignes, n=nb colonnes, can=nb canaux
+    [nbLignes, nbColonnes, can] = size(im1); % m=nb lignes, n=nb colonnes, can=nb canaux
     if(can > 1)
-        im = rgb2gray(im); % si l’image est en couleur, la transformer en NG
+        im1 = rgb2gray(im1); % si l’image est en couleur, la transformer en NG
+    end
+    [nbLignes, nbColonnes, can] = size(im2); % m=nb lignes, n=nb colonnes, can=nb canaux
+    if(can > 1)
+        im2 = rgb2gray(im2); % si l’image est en couleur, la transformer en NG
     end
 end
