604d87870c21de0e636c9b77ed9175e7265c6795
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2023-02-07 17:10:28 +0100

    [specification] Début de fonction + layout créé

---
+++ specification.m
@@ -0,0 +1,56 @@
+function Ispecification = specification (Ix, Iz)
+    [nbLignes_ix, nbColonnes_ix, can_ix] = size(Ix); % m=nb lignes, n=nb colonnes, can=nb canaux
+    if(can_ix > 1)
+        Ix = rgb2gray(Ix); % si l’image est en couleur, la transformer en NG
+    end
+
+    [nbLignes_iz, nbColonnes_iz, can_iz] = size(Iz); % m=nb lignes, n=nb colonnes, can=nb canaux
+    if(can_iz > 1)
+        Iz = rgb2gray(Iz); % si l’image est en couleur, la transformer en NG
+    end
+
+    Ispecification = zeros(nbLignes_ix, nbColonnes_ix);
+
+    % ---------
+    % affichage
+    % ---------
+    figure;
+
+    subplot(4, 2, 1); %sélectionne le premier cadran de la fenêtre
+    imshow(Ix);
+    title("Image source");
+
+    subplot(4, 2, 2); %sélectionne le deuxieme cadran de la fenêtre
+    imshow(Iz)
+    title("Image de référence");
+
+    subplot(4, 2, 3); %sélectionne le troisème cadran de la fenêtre
+    imhist(Ix);
+    axis([-inf +inf -inf +inf]);
+    title("Histogramme image source");
+
+    subplot(4, 2, 4); %sélectionne le quatrième cadran de la fenêtre
+    imhist(Iz);
+    axis([-inf +inf -inf +inf]);
+    title("Histogramme image de référence");
+
+    subplot(4, 2, 5); %sélectionne le cinquième cadran de la fenêtre
+    %imhist(Ix);
+    %axis([-inf +inf -inf +inf]);
+    title("Histogramme cumulée image source");
+
+    subplot(4, 2, 6); %sélectionne le sixième cadran de la fenêtre
+    %imhist(Iz);
+    %axis([-inf +inf -inf +inf]);
+    title("Histogramme cumulée image de référence");
+
+    subplot(4, 2, 7); %sélectionne le cinquième cadran de la fenêtre
+    imshow(Ispecification);
+    title("Image obtenue par la spécification");
+
+    subplot(4, 2, 8); %sélectionne le sixième cadran de la fenêtre
+    %imhist(Ispecification);
+    title("Histogramme de l'image obtenue par la spécification");
+    %axis([-inf +inf -inf +inf]);
+end
+
